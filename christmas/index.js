903fd3374013cb52c7912d54e89896b0af4d39ff
seo-K <dkasid@naver.com>
2023-12-11 02:09:47 +0900

    feat : toggle 이벤트 추가

--- christmas/index.js
+++ christmas/index.js
@@ -2,9 +2,8 @@ const calendarWrap = document.querySelector("main");
 const cardTemplate = document.getElementById("card-list");
 
 for (let i = 1; i < 25; i++) {
-  console.log(i);
+  // console.log(i);
   let cardEl = document.importNode(cardTemplate.content, true);
-  let cardWRaper = document.importNode;
   cardEl.querySelector(".card-wrap").style.gridArea = `d${i}`;
   cardEl.querySelector('slot[name="date"]').textContent = i;
 
@@ -18,9 +17,21 @@ for (let i = 1; i < 25; i++) {
 
   let openedCardImgSlot = cardEl.querySelector('slot[name="opened-card-img"]');
   let openedCardImg = document.createElement("img");
-  openedCardImg.src = `./image/frame/frame${i}.png`;
+  openedCardImg.src = `./image/card/card-${i}.png`;
   openedCardImg.alt = "";
   openedCardImgSlot.appendChild(openedCardImg);
 
   calendarWrap.append(cardEl); // 캘린더 안에 템플릿 넣기
 }
+
+const toggleButton = (el) => {
+  const Active = el.classList.contains("active");
+  console.log(el);
+  if (Active) {
+    el.classList.remove("active");
+    console.log("first");
+  } else {
+    el.classList.add("active");
+    console.log("2");
+  }
+};
