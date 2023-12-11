4dba0acbd67f59c07175a4db362648bf837c9139
Seo-K <77239319+seo-K@users.noreply.github.com>
2023-12-11 08:39:11 +0000

    feat: dialog popover insert 작업중

--- christmas/index.js
+++ christmas/index.js
@@ -12,13 +12,13 @@ for (let i = 1; i < 25; i++) {
   );
   let defaultCardImg = document.createElement("img");
   defaultCardImg.src = `./image/frame/frame${i}.png`;
-  defaultCardImg.alt = "";
+  defaultCardImg.alt = `${i}일`;
   defaultCardImgSlot.appendChild(defaultCardImg);
 
   let openedCardImgSlot = cardEl.querySelector('slot[name="opened-card-img"]');
   let openedCardImg = document.createElement("img");
   openedCardImg.src = `./image/card/card-${i}.png`;
-  openedCardImg.alt = "";
+  openedCardImg.alt = `${i}일`;
   openedCardImgSlot.appendChild(openedCardImg);
 
   calendarWrap.append(cardEl); // 캘린더 안에 템플릿 넣기
@@ -26,12 +26,51 @@ for (let i = 1; i < 25; i++) {
 
 const toggleButton = (el) => {
   const Active = el.classList.contains("active");
-  console.log(el);
-  if (Active) {
-    el.classList.remove("active");
-    console.log("first");
-  } else {
-    el.classList.add("active");
-    console.log("2");
-  }
+  // if (Active) {
+  //   el.classList.remove("active");
+   
+  // } else {
+  //   el.classList.add("active");
+  
+  // }
 };
+// const card = document.querySelector(".card-wrap");
+// const Active = card.classList.contains("active")
+
+// if(Active){
+//   document.querySelector("main").setAttribute('inert', '');
+// } else{
+//   document.querySelector("main").removeAttribute('inert');
+// }
+
+
+const isOpen = document.querySelector("dialog:popover-open");
+
+if(isOpen){
+  console.log("열림")
+} else{
+  console.log("닫힘")
+}
+
+
+
+// const DIALOG = document.getElementById('modal');
+// const MAIN = document.querySelector("main");
+// document.querySelector("[popovertarget]").addEventListener("click", () => {
+//   if(DIALOG.matches(':popover-open')) {
+//     console.log("열림")
+//     MAIN.removeAttribute('inert');
+//   } else{
+//     MAIN.setAttribute('inert', '');
+//   }
+//   console.log(DIALOG.matches(':popover-open'));
+// })
+// console.dir(DIALOG)
+// isInert = DIALOG.inert;
+
+
+
+// DIALOG.inert = true | false;
+// // 또는
+//  // 요소에 inert 속성 적용
+// DIALOG.removeAttribute('inert'); // 요소에 inert 속성 제거
