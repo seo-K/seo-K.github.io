14285ac987730838d97fd4538a3696abd3bc95d0
seo-K <dkasid@naver.com>
2023-12-12 02:49:06 +0900

    feat: 모달 이미지업데이트

--- christmas/index.js
+++ christmas/index.js
@@ -1,76 +1,62 @@
 const calendarWrap = document.querySelector("main");
 const cardTemplate = document.getElementById("card-list");
+const main = document.querySelector("main");
+const dialog = document.querySelector("dialog");
 
-for (let i = 1; i < 25; i++) {
-  // console.log(i);
+// Function to create a card element
+function createCard(index) {
   let cardEl = document.importNode(cardTemplate.content, true);
-  cardEl.querySelector(".card-wrap").style.gridArea = `d${i}`;
-  cardEl.querySelector('slot[name="date"]').textContent = i;
+  cardEl.querySelector(".card-wrap").style.gridArea = `d${index}`;
+  cardEl.querySelector('slot[name="date"]').textContent = index;
 
   let defaultCardImgSlot = cardEl.querySelector(
     'slot[name="default-card-img"]'
   );
   let defaultCardImg = document.createElement("img");
-  defaultCardImg.src = `./image/frame/frame${i}.png`;
-  defaultCardImg.alt = `${i}일`;
+  defaultCardImg.src = `./image/frame/frame${index}.png`;
+  defaultCardImg.alt = `${index}일`;
   defaultCardImgSlot.appendChild(defaultCardImg);
 
   let openedCardImgSlot = cardEl.querySelector('slot[name="opened-card-img"]');
   let openedCardImg = document.createElement("img");
-  openedCardImg.src = `./image/card/card-${i}.png`;
-  openedCardImg.alt = `${i}일`;
+  openedCardImg.src = `./image/card/card-${index}.png`;
+  openedCardImg.alt = `${index}일`;
   openedCardImgSlot.appendChild(openedCardImg);
 
-  calendarWrap.append(cardEl); // 캘린더 안에 템플릿 넣기
+  return cardEl;
 }
 
+// Populate the calendar with cards
+for (let i = 1; i < 25; i++) {
+  let cardEl = createCard(i);
+  calendarWrap.append(cardEl);
+}
+
+// Function to toggle the modal and handle card activation
 const toggleButton = (el) => {
-  const Active = el.classList.contains("active");
-  // if (Active) {
-  //   el.classList.remove("active");
-   
-  // } else {
-  //   el.classList.add("active");
-  
-  // }
+  const cardWrap = el.closest(".card-wrap");
+  const isActive = cardWrap.classList.contains("active");
+  let currentIndex;
+
+  currentIndex =
+    Array.from(main.querySelectorAll(".card-wrap")).indexOf(cardWrap) + 1;
+
+  const figureElement = dialog.querySelector("figure");
+  figureElement.innerHTML = "";
+
+  if (!isActive) {
+    let modalCardImg = document.createElement("img");
+    modalCardImg.src = `./image/card/card-${currentIndex}.png`;
+    modalCardImg.alt = `${currentIndex}일`;
+    figureElement.appendChild(modalCardImg);
+
+    dialog.showModal();
+    cardWrap.classList.add("active");
+  } else {
+    cardWrap.classList.remove("active");
+  }
 };
-// const card = document.querySelector(".card-wrap");
-// const Active = card.classList.contains("active")
 
-// if(Active){
-//   document.querySelector("main").setAttribute('inert', '');
-// } else{
-//   document.querySelector("main").removeAttribute('inert');
-// }
-
-
-const isOpen = document.querySelector("dialog:popover-open");
-
-if(isOpen){
-  console.log("열림")
-} else{
-  console.log("닫힘")
-}
-
-
-
-// const DIALOG = document.getElementById('modal');
-// const MAIN = document.querySelector("main");
-// document.querySelector("[popovertarget]").addEventListener("click", () => {
-//   if(DIALOG.matches(':popover-open')) {
-//     console.log("열림")
-//     MAIN.removeAttribute('inert');
-//   } else{
-//     MAIN.setAttribute('inert', '');
-//   }
-//   console.log(DIALOG.matches(':popover-open'));
-// })
-// console.dir(DIALOG)
-// isInert = DIALOG.inert;
-
-
-
-// DIALOG.inert = true | false;
-// // 또는
-//  // 요소에 inert 속성 적용
-// DIALOG.removeAttribute('inert'); // 요소에 inert 속성 제거
+const closeModal = (el) => {
+  dialog.close();
+};
