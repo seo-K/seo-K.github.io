aba86d49251b7126a505502d37de2c990d56555d
Seo-K <77239319+seo-K@users.noreply.github.com>
2023-12-27 01:15:09 +0000

    feat: project 섹션 마무리 및 스와이퍼 작업중

--- simple/assets/js/common.js
+++ simple/assets/js/common.js
@@ -5,19 +5,19 @@ const windowHeight = window.innerHeight;
 const observeElements = document.querySelectorAll(".visual > article");
 const stickyContent = document.querySelector(".sticky-content");
 
-function handleArticleScroll() {
+const handleArticleScroll = () => {
   observeElements.forEach((item, index) => {
     const bottom = item.getBoundingClientRect().bottom;
     stickyContent.classList.toggle(`active_${index}`, 0 < bottom && bottom < item.getBoundingClientRect().height);
   });
-}
+};
 
-// PROJECT PROGRESS
+// PROJECT SCROLL
 const projectWrap = document.querySelector(".project");
 const projectList = document.querySelectorAll(".project-list");
 const headerHeight = document.querySelector("#header").getBoundingClientRect().height * 2;
 
-function handleProjectScroll() {
+const handleProjectScroll = () => {
   if (projectWrap.getBoundingClientRect().top < 0 && windowHeight < projectWrap.getBoundingClientRect().bottom) {
     const targetScroll = -projectWrap.getBoundingClientRect().top;
 
@@ -37,12 +37,40 @@ function handleProjectScroll() {
       }
     });
   }
-}
+};
+
+// CODE SCROLL
+const codeWrap = document.querySelector(".code");
+const codeButton = document.querySelectorAll(".code__button-list button");
+
+const handleCodeScroll = () => {
+  if (codeWrap.getBoundingClientRect().top < 0 && windowHeight < codeWrap.getBoundingClientRect().bottom) {
+    const targetScroll = -codeWrap.getBoundingClientRect().top;
+
+    function setGradient(el, percentage) {
+      const gradientValue = `linear-gradient(90deg, white ${percentage}%, transparent 0%)`;
+      el.style.background = gradientValue;
+    }
+
+    codeButton.forEach((item, index) => {
+      const startScroll = windowHeight * index;
+      const endScroll = windowHeight * (index + 1);
+
+      setGradient(item, 0);
+
+      if (startScroll < targetScroll && targetScroll < endScroll) {
+        const progressValue = ((targetScroll - startScroll) / windowHeight) * 100;
+        setGradient(item, progressValue);
+      }
+    });
+  }
+};
 
 // 스크롤 이벤트 리스너
 function scrollHandler() {
   handleArticleScroll();
   handleProjectScroll();
+  handleCodeScroll();
 }
 
 // 스크롤 이벤트에 딜레이 적용하여 최적화
@@ -59,3 +87,21 @@ function throttledScrollHandler() {
 
 // 스크롤 이벤트 리스너 등록
 window.addEventListener("scroll", throttledScrollHandler);
+
+projectList.forEach((item, index) => {
+  item.addEventListener("click", () => scrollToList(index));
+});
+
+const scrollToList = (index) => {
+  // +1 은 넘어가야 details 가 open 되어서 추가함.
+  let y = windowHeight * (index + 1) + 1;
+  window.scrollTo(0, y);
+};
+
+const onClickVideoBtn = (el) => {
+  const videoWrap = el.closest(".sticky-content__video");
+  let mobileMode = videoWrap.classList.contains("mobile-mode");
+
+  videoWrap.classList.toggle("mobile-mode");
+  el.innerText = mobileMode ? "MOBILE" : "PC";
+};
